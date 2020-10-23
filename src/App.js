adcabeed0f65d53c9a542d9b27c1df0af854781b
泰知 <t1a2i3t4i5@gmail.com>
2020-10-24 06:20:24 +0900

    create classcomponents and functional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 
-function App() {
+// class App extends Component{
+//   render(){
+//   return (
+//     <React.Fragment>
+//       <h1>佐藤て泰知です</h1>
+//     </React.Fragment>
+//   )
+//   }
+// }
+
+
+const App = () => {
   return (
     <React.Fragment>
-      <label htmlFor="hoge">hoge</label>
-      <input type="text" onClick={() => console.log("クリックされました")} />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
     </React.Fragment>
   )
 }
+
+const Cat = () => {
+  return <div>Mewo</div>
+}
+
 
 export default App;
