ba5d301e71656bb1a424292d1a03ecd292d011d9
泰知 <t1a2i3t4i5@gmail.com>
2020-10-25 21:58:08 +0900

    implement store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,9 @@
-const INCREMENT ='INCREMENT'
-const DECREMENT = 'DECREMENT'
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
 export const increment = () => ({
 		type: INCREMENT
 })
-
-export const decrement = () =>({
+export const decrement = () => ({
 		type: DECREMENT
 })
